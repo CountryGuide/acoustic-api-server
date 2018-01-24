08e17443daffb9ad44061c0b6839f78595a00726
yakovlev <ivanvyakovlev@gmail.com>
2018-01-24 13:46:45 +0300

    added test route

--- acoustic/urls.py
+++ acoustic/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import url
 
 from django.contrib import admin
+from acoustic.API.views import TestView
 admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('^admin/?', admin.site.urls),
+    re_path('^api/?', TestView.as_view())
 ]
