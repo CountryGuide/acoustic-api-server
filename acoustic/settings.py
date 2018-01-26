f96e62d4eb74885f764ac81c7c7bfe67f44a25ce
yakovlev <ivanvyakovlev@gmail.com>
2018-01-26 19:08:03 +0300

    added corsheaders

--- acoustic/settings.py
+++ acoustic/settings.py
@@ -41,12 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'acoustic.API'
+    'acoustic.API',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -104,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'https://acoustic.herokuapp.com/'
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
